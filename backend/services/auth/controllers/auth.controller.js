aee71c599947b39837ce3e55b888560da0f57830
techapoorv33 <techapoorv33@gmail.com>
2026-09-14 23:11:58 +0530

    google auth api completed

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -4,8 +4,9 @@ export const login= async (req,res)=>{
     try {
         const {token}=req.body
         const decoded=getAuth(app).verifyIdToken(token)
-        
+        console.log(decoded)
+        return res.json({decoded})
     } catch (error) {
-        
+        return res.status(500).json({message:`login error:${error}`})
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import { auth, googleProvider } from '../firebase'
 
 const App = () => {
+  const handleLogin= async ()=>{
+    const data=await signInWithPopup(auth,googleProvider)
+    const token = await data.user.getIdToken()
+  }
+
   return (
     <div>
-      <button>continue with google</button>
+      <button onClick={handleLogin}>continue with google</button>
     </div>
   )
 }
